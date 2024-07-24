93f1c53566eadfa30453b0780ced926db6895dce
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-07-24 11:47:05 +0300

    implement isPackagePublished

--- src/lib/npm/registry.ts
+++ src/lib/npm/registry.ts
@@ -4,11 +4,11 @@ export interface NpmOptions {
 	registry?: string;
 }
 
-function getNpmCliArgsFromOptions(options: NpmOptions): string {
-	let cliArgs = "";
+function getNpmCliArgsFromOptions(options: NpmOptions): string[] {
+	const cliArgs: string[] = [];
 
 	if (options.registry) {
-		cliArgs += ` --registry ${options.registry}`;
+		cliArgs.push("--registry", options.registry);
 	}
 
 	return cliArgs;
@@ -24,10 +24,50 @@ export async function isPackagePublished({
 	options?: NpmOptions;
 }): Promise<boolean> {
 	const extraCliArgs = getNpmCliArgsFromOptions(options);
-	const { stdout } = await execa(
-		`npm view ${packageName}@${version}${extraCliArgs}`,
+	const { stdout, stderr } = await execa(
+		"npm",
+		["view", `${packageName}@${version}`, "--json", ...extraCliArgs],
+		{
+			// Don't throw on process failure
+			reject: false,
+		},
 	);
-	return stdout !== "";
+
+	// biome-ignore lint/suspicious/noExplicitAny: we don't know the type if it's not an error or something
+	let parsed: any;
+
+	try {
+		parsed = JSON.parse(stdout);
+	} catch (e) {
+		console.error("Failed to parse JSON stdout", {
+			stdout,
+			stderr,
+			parseJsonError: e,
+		});
+
+		throw e;
+	}
+
+	const errorCode = parsed?.error?.code;
+
+	// Not published
+	if (errorCode === "E404") {
+		return false;
+	}
+
+	if (errorCode) {
+		let errorMessageFromOutput: string;
+		if (parsed.error.summary) {
+			errorMessageFromOutput = parsed.error.summary;
+			errorMessageFromOutput += parsed.error.detail || "";
+		} else {
+			errorMessageFromOutput = `Unknown error\n\nSTDOUT:\n${stdout}\nSTDERR:\n${stderr}`;
+		}
+
+		throw new Error(errorMessageFromOutput);
+	}
+
+	return true;
 }
 
 export async function isPublishedPackageLatest({
@@ -40,10 +80,15 @@ export async function isPublishedPackageLatest({
 	options?: NpmOptions;
 }): Promise<boolean> {
 	const extraCliArgs = getNpmCliArgsFromOptions(options);
-	const { stdout } = await execa(
-		`npm dist-tag ls ${packageName}${extraCliArgs}`,
+	const { all } = await execa(
+		"npm",
+		["dist-tag", "ls", `${packageName}@${version}`, ...extraCliArgs],
+		{
+			all: true,
+		},
 	);
-	const tags = stdout.split("\n");
+
+	const tags = all.split("\n");
 
 	return tags.includes(`latest: ${version}`);
 }
@@ -58,8 +103,13 @@ export async function markPublishedPackageAsLatest({
 	options?: NpmOptions;
 }) {
 	const extraCliArgs = getNpmCliArgsFromOptions(options);
+
 	await execa(
-		`npm dist-tag add ${packageName}@${version} latest${extraCliArgs}`,
+		"npm",
+		["dist-tag", "add", `${packageName}@${version}`, "latest", ...extraCliArgs],
+		{
+			all: true,
+		},
 	);
 }
 
@@ -75,11 +125,17 @@ export async function publishPackage({
 	options?: NpmOptions;
 }) {
 	const extraCliArgs = getNpmCliArgsFromOptions(options);
-	let tagString = "";
+	const tagOptions: string[] = [];
 
 	if (!setLatest) {
-		tagString = `--tag ${packageName}@${version}`;
+		tagOptions.push("--tag", `${packageName}@${version}`);
 	}
 
-	await execa(`npm publish ${tagString}${extraCliArgs}`);
+	await execa(
+		"npm",
+		["publish", `${packageName}@${version}`, ...tagOptions, ...extraCliArgs],
+		{
+			all: true,
+		},
+	);
 }
