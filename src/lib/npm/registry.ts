1c8bcd6edc5cdab14529f11ae06a3d89bcc7a418
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2024-07-24 11:25:04 +0300

    format

--- src/lib/npm/registry.ts
+++ src/lib/npm/registry.ts
@@ -1,85 +1,85 @@
-import {execa} from "execa";
+import { execa } from "execa";
 
 export interface NpmOptions {
-    registry?: string;
+	registry?: string;
 }
 
 function getNpmCliArgsFromOptions(options: NpmOptions): string {
-    let cliArgs = "";
+	let cliArgs = "";
 
-    if (options.registry) {
-        cliArgs += ` --registry ${options.registry}`;
-    }
+	if (options.registry) {
+		cliArgs += ` --registry ${options.registry}`;
+	}
 
-    return cliArgs;
+	return cliArgs;
 }
 
 export async function isPackagePublished({
-                                             packageName,
-                                             version,
-                                             options = {},
-                                         }: {
-    packageName: string;
-    version: string;
-    options?: NpmOptions;
+	packageName,
+	version,
+	options = {},
+}: {
+	packageName: string;
+	version: string;
+	options?: NpmOptions;
 }): Promise<boolean> {
-    const extraCliArgs = getNpmCliArgsFromOptions(options);
-    const {stdout} = await execa(
-        `npm view ${packageName}@${version}${extraCliArgs}`,
-    );
-    return stdout !== "";
+	const extraCliArgs = getNpmCliArgsFromOptions(options);
+	const { stdout } = await execa(
+		`npm view ${packageName}@${version}${extraCliArgs}`,
+	);
+	return stdout !== "";
 }
 
 export async function isPublishedPackageLatest({
-                                                   packageName,
-                                                   version,
-                                                   options = {},
-                                               }: {
-    packageName: string;
-    version: string;
-    options?: NpmOptions;
+	packageName,
+	version,
+	options = {},
+}: {
+	packageName: string;
+	version: string;
+	options?: NpmOptions;
 }): Promise<boolean> {
-    const extraCliArgs = getNpmCliArgsFromOptions(options);
-    const {stdout} = await execa(
-        `npm dist-tag ls ${packageName}${extraCliArgs}`,
-    );
-    const tags = stdout.split("\n");
+	const extraCliArgs = getNpmCliArgsFromOptions(options);
+	const { stdout } = await execa(
+		`npm dist-tag ls ${packageName}${extraCliArgs}`,
+	);
+	const tags = stdout.split("\n");
 
-    return tags.includes(`latest: ${version}`);
+	return tags.includes(`latest: ${version}`);
 }
 
 export async function markPublishedPackageAsLatest({
-                                                       packageName,
-                                                       version,
-                                                       options = {},
-                                                   }: {
-    packageName: string;
-    version: string;
-    options?: NpmOptions;
+	packageName,
+	version,
+	options = {},
+}: {
+	packageName: string;
+	version: string;
+	options?: NpmOptions;
 }) {
-    const extraCliArgs = getNpmCliArgsFromOptions(options);
-    await execa(
-        `npm dist-tag add ${packageName}@${version} latest${extraCliArgs}`,
-    );
+	const extraCliArgs = getNpmCliArgsFromOptions(options);
+	await execa(
+		`npm dist-tag add ${packageName}@${version} latest${extraCliArgs}`,
+	);
 }
 
 export async function publishPackage({
-                                         packageName,
-                                         version,
-                                         setLatest,
-                                         options = {},
-                                     }: {
-    packageName: string;
-    version: string;
-    setLatest: boolean;
-    options?: NpmOptions;
+	packageName,
+	version,
+	setLatest,
+	options = {},
+}: {
+	packageName: string;
+	version: string;
+	setLatest: boolean;
+	options?: NpmOptions;
 }) {
-    const extraCliArgs = getNpmCliArgsFromOptions(options);
-    let tagString = "";
+	const extraCliArgs = getNpmCliArgsFromOptions(options);
+	let tagString = "";
 
-    if (!setLatest) {
-        tagString = `--tag ${packageName}@${version}`;
-    }
+	if (!setLatest) {
+		tagString = `--tag ${packageName}@${version}`;
+	}
 
-    await execa(`npm publish ${tagString}${extraCliArgs}`);
+	await execa(`npm publish ${tagString}${extraCliArgs}`);
 }
